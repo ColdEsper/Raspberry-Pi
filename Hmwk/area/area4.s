66c9bce9d038c1f3c1f63522b21e65e378d968a2
ColdEsper <shadowguardian@live.com>
2015-09-23 13:15:44 -0700

    fix to area4.s

--- Hmwk/area/area4.s
+++ Hmwk/area/area4.s
@@ -1,10 +1,11 @@
 	.global _start
 _start:
-	movw R1, #0b0110010010010010 @ lower half bp -13 3 bytes approximately pi 
+	movw R1, #0b1001001001001001 @ lower half bp -21 3 bytes approximately pi 
+	movt R1, #0b0000000001100100 @ top half bp -21 bytes approximately pi 
 	mov R2, #0b110 @ bp 0 1 byte radius of 6
 	mul R3, R2, R2
 	mul R0, R1, R3
-	lsr R0, R0, #13
+	lsr R0, R0, #21
 
 	mov R7, #1
 	swi 0
