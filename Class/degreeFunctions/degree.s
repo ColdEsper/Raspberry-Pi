7541e27daf955d393891e635a76f5c649a6c7e3a
Casey Copeland <shadowguardian@live.com>
2015-10-19 12:25:39 -0700

    more 10/19 progress

--- Class/degreeFunctions/degree.s
+++ Class/degreeFunctions/degree.s
@@ -6,9 +6,9 @@ input_msg: .asciz "Input beginning and end of temperature range.\n"
 .balign 4
 range_input_msg: .asciz "If range is degree Centigrade inut 1\nIf range is degree Fahrenheiht input 2\n"
 .balign 4
-return: .word 0 .text
+return: .word 0 
 .balign 4
-return2: .word 0 .text
+return2: .word 0
 .text
 	.global main
 main:
@@ -21,21 +21,20 @@ main:
 	ldr LR, [LR]
 	bx LR
 
-.function problemTwo
 problemTwo:
 	/*save address of return */
 	ldr R1, address_of_return2
 	str LR, [R1]
 
 	/* print msg*/
-	ldr R0, display_msg
+	ldr R0, =display_msg
 	bl printf
 
 	/*return */
 	bx LR
 
 address_of_return: .word return
-address_of_return: .word return2
+address_of_return2: .word return2
 
 .global printf
-.gloabl scanf
+.global scanf
