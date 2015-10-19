d9f5785abcf71b3a75ff9848a414a17bf24efaf2
ColdEsper <shadowguardian@live.com>
2015-10-19 15:31:39 -0700

    celsius to fahrenheit progress

--- Hmwk/degreeFunctions/degree.s
+++ Hmwk/degreeFunctions/degree.s
@@ -1,6 +1,6 @@
 .data
 .balign 4
-display_msg: .asciz "Display Degree Centigrade to Degree Fahrenheight\nFahrenheit Centigrade\n"
+display_msg: .asciz "Display Degree Centigrade to Degree Fahrenheit\nFahrenheit Centigrade\n"
 .balign 4
 input_msg: .asciz "Input beginning and end of temperature range.\n"
 .balign 4
@@ -8,12 +8,20 @@ range_input_msg: .asciz "If range is degree Centigrade input 1\nIf range is degr
 .balign 4
 inputFormat: .asciz "%d"
 .balign 4
+printFormat: .asciz "%d\n"
+.balign 4
 start: .word 0
 .balign 4
 end: .word 0
 .balign 4
 choice: .word 0
 .balign 4
+counter: .word 0
+/*fahrenheit and celsius at binary point 7 */
+.balign 4
+fahrenheit: .word 0
+.balign 4
+celsius: .word 0
 return: .word 0 
 .balign 4
 return2: .word 0 
@@ -43,7 +51,7 @@ convAsk:
 	bl scanf
 	ldr R1, =choice
 	ldr R1, [R1]
-	/* jump to conversion function, else repeat quesition */
+	/* jump to conversion function, jump to quit, or repeat quesition */
 	cmp R1, #1
 	beq doOne
 	cmp R1, #2
@@ -63,6 +71,7 @@ endDo:
 	ldr LR, [R1]
 	bx LR
 
+/*Celsius to Fahrenheit*/
 problemOne:
 	/*save address of return */
 	ldr R1, address_of_return2
@@ -71,12 +80,49 @@ problemOne:
 	/*print msg*/
 	ldr R0, =display_msg
 	bl printf
+	ldr R3, =start
+	ldr R3, [R3]
+	ldr R6, =counter
+	str R3, [R6]
+	ldr R5, =end
+	ldr R4, [R5]
+	add R4, #1
+	str R4, [R5]
+	b convertCtoFStart
+convertCtoF:
+	ldr R3, =counter
+	ldr R3, [R3]
+	ldr R4, =end
+	ldr R4, [R4]
+convertCtoFStart:
+	cmp R3, R4
+	beq endConvertCtoF
+	mov R2, R3
+	/*now scales up R2 to bp 7*/
+	lsl R2, #7
+	ldr R5, =nineFifths
+	ldr R5, [R5]
+	mul R2, R5
+	/*now scales down R2 from bp 7 to bp 0*/
+	lsr R2, #7
+	add R2, #32
+	/*increment counter R3*/
+	add R3, #1
+	ldr R6, =counter
+	str R3, [R6]
+	/*print conversion*/
+	ldr R0, =printFormat
+	mov R1, R2
+	bl printf
+	b convertCtoF
+endConvertCtoF:
 
 	/*return */
 	ldr R1, address_of_return2
 	ldr LR, [R1]
 	bx LR
 
+/*Fahrenheit to Celsius*/
 problemTwo:
 	/*save address of return */
 	ldr R1, address_of_return2
@@ -93,6 +139,10 @@ problemTwo:
 
 address_of_return: .word return
 address_of_return2: .word return2
+/* 5.0/9.0 at binary point 7 */
+fiveNinths: .word 0x47
+/* 9.0/5.0 at binary point 7 */
+nineFifths: .word 0xe6
 
 .global printf
 .global scanf
